aa0e8a5ed5ff5f8485b598ae79caf21d7727ba3d
mattRCB <mattsteinrcb@gmail.com>
2016-11-15 14:31:33 -0500

    Progress on js for minimal viable product

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,71 +1,3 @@
-
-
-var words = ["federer", "becker", "agassi"]
-/* make an array from string at index0 of words */
-var currentWord = words[0].split("");
-var currentBlanks = [];
-var guessesRemaining = 8;
-var lettersGuessed = [];
-
-
-/* Write letter blanks to the screen */
-for (var i = 0; i < currentWord.length; i++) {
-	currentBlanks.push("_ ");
-	document.getElementById("displayBlanks").innerHTML += "_ ";
-}
-console.log(currentBlanks);
-console.log(currentWord);
-
-/* Write # of guesses remaining to the screen */
-document.getElementById("displayRemaining").innerHTML = guessesRemaining;
-
-
-
-document.onkeyup = function(event) {
-			var key = String.fromCharCode(event.keyCode).toLowerCase();
-			console.log(key);
-			var foundTheLetter = false;
-			
-			/* For loop to apply .indexof to each element of currentWord */
-			for (var i = 0; i < currentWord.length; i++) {
-				/* If the letter guessed is in the word... */
-				if (key == currentWord[i]) {
-					/* add the letter into the correct index of currentBlanks array */
-					currentBlanks[i] = key + " ";
-					foundTheLetter = true;
-				}
-			}
-
-			if (foundTheLetter == false) {
-				guessesRemaining--;
-				document.getElementById("displayRemaining").innerHTML = guessesRemaining;
-				lettersGuessed.push(key);
-				document.getElementById("displayGuessed").innerHTML = lettersGuessed;
-			} else {
-				document.getElementById("displayBlanks").innerHTML = currentBlanks.join("");
-			}
-					
-					
-
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
 var randomSelector = ["federer", "becker", "agassi"]
 
@@ -83,24 +15,70 @@ var gameObject = {
 		img : "./assets/images/agassi.jpg"
 	}	
 }
-
-
-
-		document.onkeyup = function(event) {
-			var keyPressed = String.fromCharCode(event.keyCode).toLowerCase();
-			console.log(keyPressed);
-			var currentWord = gameObject.federer.word;
-			document.getElementById("displayWord").innerHTML = currentWord;
-
-
-			
-		};
-
-
 */
 
+var words = ["federer", "becker", "agassi"]
+var currentWord = [];
+var currentBlanks = [];
+var guessesRemaining = 8;
+var lettersGuessed = [];
+
+
+/* Choose random string from words array; split it into currentWord array */
+currentWord = words[Math.floor(Math.random() * words.length)].split("");
+/* STILL NEED TO remove the value the user has selected and then return a random element from the diminished array. PROBABLY SHOULD MAKE THIS A FUNCTION TO BE CALLED HERE AND FROM END OF FLOW TO RESET GAME. */
+
+
+/* Set currentBlanks based on currentWord; Draw blanks on screen */
+for (var i = 0; i < currentWord.length; i++) {
+	currentBlanks.push("_ ");
+	document.getElementById("displayBlanks").innerHTML = currentBlanks.join("");
+}
+// console.log(currentBlanks);
+// console.log(currentWord);
+
+
+/* Write initial # of guesses remaining to the screen */
+document.getElementById("displayRemaining").innerHTML = guessesRemaining;
+
+
+
+document.onkeyup = function(event) {
+	var key = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log(key);
+	var foundTheLetter = false;
+	
+	/* Iterate thru currentWord array checking for letter-key pressed */
+	for (var i = 0; i < currentWord.length; i++) {
+		/* If the letter guessed is in the word... */
+		if (key == currentWord[i]) {
+			/* add the letter at the correct index of currentBlanks array */
+			currentBlanks[i] = key + " ";
+			foundTheLetter = true;
+		}
+	}
+
+	if (foundTheLetter == false) {
+		guessesRemaining--;
+		document.getElementById("displayRemaining").innerHTML = guessesRemaining;
+		lettersGuessed.push(key);
+		document.getElementById("displayGuessed").innerHTML = lettersGuessed;
+		/* Check if guessesRemaining is 0; if so, end round & reset for new round. */
+
+	} else /* THE LETTER-GUESSED WAS CORRECT */ {
+		/*Add the letter to the on-screen blanks/letters display */
+		document.getElementById("displayBlanks").innerHTML = currentBlanks.join("");
+		/* Check if all blanks are filled-in; if so: Increment Wins; Display picture, etc.; Reset for new round. */
+
+	}
+					
+					
+
+
+};
 
 
 
 
-		// document.getElementById("test").innerHTML = "Press any key to get started";
+
+
